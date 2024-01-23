ba71d877974498a039c04223455710f0454b1e97
Abdugaffor <abdugafforlutfillaev14@gmail.com>
2024-01-23 13:35:47 +0300

    fixed bugs

--- app/tags/[tagId]/page.jsx
+++ app/tags/[tagId]/page.jsx
@@ -1,8 +1,8 @@
 'use client'
 import { useEffect } from "react";
-import TagsVideoSection from '../components/tagVideosSection'
+import TagsVideoSection from '../../components/tagVideosSection'
 import { useDispatch, useSelector } from "react-redux";
-import { getTagsDataAsync } from "../../redux/slices/tagsSlice";
+import { getTagsDataAsync } from "../../../redux/slices/tagsSlice";
 
 
 function tagVideosPage({params}){
