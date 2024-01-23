631cfdfc4e69ce0e6aa38b9655d473b86618f03d
Abdugaffor <abdugafforlutfillaev14@gmail.com>
2024-01-23 14:50:12 +0300

    fixed

--- pages/api/tags.js
+++ pages/api/tags.js
@@ -1,37 +1,37 @@
-import prisma from '../../lib/prisma'
+import prisma from '../../lib/prisma';
 
+export default async function handler(req, res) {
+    const method = req.method;
 
-export default async function handler(req, res){
-    const method = req.method
-
-    if(method === "GET"){
-        const data = await prisma.tag.findMany({
-            include:{
-                videos:{
-                    select:{id: true, title: true, description:true, url: true }
+    try {
+        if (method === "GET") {
+            const data = await prisma.tag.findMany({
+                include: {
+                    videos: {
+                        select: { id: true, title: true, description: true, url: true }
+                    }
                 }
-            }
-        })
-        res.status(200).json(data)
+            });
+            res.status(200).json(data);
+        } else if (method === "POST") {
+            const { name } = req.body;
+
+            const result = await prisma.tag.create({
+                data: {
+                    name
+                }
+            });
+
+            res.status(200).json({ result });
+        } else {
+            // Вместо использования `return` просто завершим выполнение кода здесь
+            res.status(405).json({ error: `Method ${method} is not allowed` });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+        // Завершаем ответ после завершения выполнения кода
+        res.end();
     }
-
-    if(method === "POST"){
-        const {name} = req.body
-
-        const result = await prisma.tag.create({
-            data:{
-                name
-            }
-        })
-
-        return res.status(200).json({result})
-    }
-
-    return res.status(405).json({error: `Method ${method} is not alllowed`})
-
-
-    // if(req.method === 'GET'){
-    //     const data = await prisma.video.findMany()
-    //     return res.status(200).json(data)
-    // }
 }
